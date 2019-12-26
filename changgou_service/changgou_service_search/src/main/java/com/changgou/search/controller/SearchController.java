20f85a9cd1af4b55edf501e0115158d1a02f1ff2
wangxiaoxiao <wangxiaoxiao@itcast.com>
2019-12-26 22:36:35 +0800

    修改的searchController里面的方法

--- changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
+++ changgou_service/changgou_service_search/src/main/java/com/changgou/search/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.changgou.search.controller;
 
+import com.changgou.common.entity.Page;
+import com.changgou.search.pojo.SkuInfo;
 import com.changgou.search.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,6 +24,35 @@ public class SearchController {
         model.addAttribute("result",resultMap);
         model.addAttribute("searchMap",searchMap);
 
+        // 封装分页数据并返回: 总记录数,当前页,每页显示的条数
+        Page<SkuInfo> page = new Page<>(
+                Long.parseLong(String.valueOf(resultMap.get("total"))),
+                Integer.parseInt(String.valueOf(resultMap.get("pageNum"))),
+                Page.pageSize  // 这里使用的是默认值
+        );
+        model.addAttribute("page",page);
+
+        //拼装url超链接的路径
+        StringBuilder url = new StringBuilder("/search/list");
+        // 判断查询的条件是否为空
+        if(searchMap !=null && searchMap.size()>0){
+            // 判断并拼接查询条件
+            url.append("?");
+            for (String paramKey : searchMap.keySet()){
+                // 拼接key和value
+                if(!"sortRule".equals(paramKey) && !"sortField".equals(paramKey) && !"pageNum".equals(paramKey)){
+                    url.append(paramKey).append("=").append(searchMap.get(paramKey)).append("&");
+                }
+            }
+            // 将拼接好的内容转换成字符串
+            String urlString = url.toString();
+            // 去除掉路径上面的最后一个&
+            urlString = urlString.substring(0, urlString.length() - 1);
+            model.addAttribute("url",urlString);
+        }else{
+            // 如果没有查询条件的话,就直接原来的路径
+            model.addAttribute("url",url);
+        }
 
         return "search"; // 跳转到的是这个search.html的页面
     }
